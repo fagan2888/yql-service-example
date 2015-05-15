77daa0aa694192770b40c2aa20c580cb18cfa55f
josuebrunel <josuebrunel@gmail.com>
2015-05-15 23:10:51 +0200

    script to run server added

---
+++ runserver.sh
@@ -0,0 +1,11 @@
+##################################################
+#
+#   Author          : josuebrunel
+#   Filename        : runserver.sh
+#   Description     :
+#   Creation Date   : 15-05-2015
+#   Last Modified   : Fri 15 May 2015 11:00:42 PM CEST
+#
+##################################################
+
+python run.py runserver -h 0.0.0.0 -p 5000
