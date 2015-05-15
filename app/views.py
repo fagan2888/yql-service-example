d7834ba7c8e639c0e29477fd7cb179c550343f83
josuebrunel <josuebrunel@gmail.com>
2015-05-16 00:35:24 +0200

    /get all OK

--- app/views.py
+++ app/views.py
@@ -4,15 +4,27 @@
 #   Filename        :
 #   Description     :
 #   Creation Date   : 15-05-2015
-#   Last Modified   : Fri 15 May 2015 11:07:25 PM CEST
+#   Last Modified   : Fri 15 May 2015 11:53:18 PM CEST
 #
 ##################################################
 import pdb
+import json
 import flask
 from flask import request
 from app import app, db
 
 from models import User
+
+@app.route('/get')
+def select():
+    """
+    """
+    if request.method == "GET":
+        users = User.query.all()
+        results = [ u.to_dict() for u in users ]
+        return flask.jsonify({'count': len(results), 'result' : results})
+    else:
+        return flask.jsonify({'error':'Invalid method'})
 
 @app.route('/new')
 def new():
