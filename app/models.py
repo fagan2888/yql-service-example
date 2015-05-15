2513a6acd8636f4b19ffe35cf7ed25956f6607e0
josuebrunel <josuebrunel@gmail.com>
2015-05-15 23:04:55 +0200

    Fixing issue with Model

--- app/models.py
+++ app/models.py
@@ -4,14 +4,14 @@
 #   Filename        :
 #   Description     :
 #   Creation Date   : 15-05-2015
-#   Last Modified   : Fri 15 May 2015 10:03:04 PM CEST
+#   Last Modified   : Fri 15 May 2015 10:43:41 PM CEST
 #
 ##################################################
 
 from app import db
 
 
-class User(db.models):
+class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32),index=True, unique=True)
