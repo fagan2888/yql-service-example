dd20da3f977e79a2dcbf63588c4d7339ff2b6d75
josuebrunel <josuebrunel@gmail.com>
2015-05-15 23:10:30 +0200

    /new method OK

--- app/views.py
+++ app/views.py
@@ -4,14 +4,25 @@
 #   Filename        :
 #   Description     :
 #   Creation Date   : 15-05-2015
-#   Last Modified   : Fri 15 May 2015 10:13:54 PM CEST
+#   Last Modified   : Fri 15 May 2015 11:07:25 PM CEST
 #
 ##################################################
+import pdb
 import flask
+from flask import request
 from app import app, db
+
+from models import User
 
 @app.route('/new')
 def new():
     """Create new user
     """
-    return flask.jsonify({'name':'Josh'})
+    if request.method == 'GET':
+        params = request.args.to_dict()
+        u = User(**params)
+        db.session.add(u)
+        db.session.commit()
+        return flask.jsonify(**params)
+    else:
+        return flask.jsonify({'error':'Invalid method'})
