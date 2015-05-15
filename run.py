ac0b90c25a7bc7890ed0c5e5777d5f40b4b4dc29
josuebrunel <josuebrunel@gmail.com>
2015-05-16 00:01:10 +0200

    if main for run.py

--- run.py
+++ run.py
@@ -4,11 +4,11 @@
 #   Filename        : run.py
 #   Description     :
 #   Creation Date   : 15-05-2015
-#   Last Modified   : Fri 15 May 2015 10:57:13 PM CEST
+#   Last Modified   : Sat 16 May 2015 12:00:51 AM CEST
 #
 ##################################################
 
 from app import app, manager
 
-#app.run(debug=True, host='0.0.0.0')
-manager.run()
+if '__main__' == '__name__':
+    manager.run()
