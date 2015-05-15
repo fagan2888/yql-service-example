30c47b89b23ab7992f0d1f5bde38385a2ad85788
josuebrunel <josuebrunel@gmail.com>
2015-05-15 22:17:08 +0200

    user model added

---
+++ app/models.py
@@ -0,0 +1,19 @@
+##################################################
+#
+#   Author          : josuebrunel
+#   Filename        :
+#   Description     :
+#   Creation Date   : 15-05-2015
+#   Last Modified   : Fri 15 May 2015 10:03:04 PM CEST
+#
+##################################################
+
+from app import db
+
+
+class User(db.models):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32),index=True, unique=True)
+    age = db.Column(db.Integer)
+    sex = db.Column(db.CHAR(1))
