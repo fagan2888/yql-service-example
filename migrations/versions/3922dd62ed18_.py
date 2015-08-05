1731b9cd558de18d151c359879cc6fd43873bb06
Josue Kouka <josuebrunel@gmail.com>
2015-08-05 11:56:44 +0200

    migrations added

---
+++ migrations/versions/3922dd62ed18_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 3922dd62ed18
+Revises: 8ea54d0af0
+Create Date: 2015-08-05 11:53:45.962684
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3922dd62ed18'
+down_revision = '8ea54d0af0'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
